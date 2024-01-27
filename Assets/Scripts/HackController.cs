2999cf14edd68475cef94ee90b221a85d3d75ce0
Dimitrios <dimitriosbroumas@gmai.com>
2024-01-27 16:32:45 -0500

    Scene changes from sabotage

--- Assets/Scripts/HackController.cs
+++ Assets/Scripts/HackController.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using System;
+using UnityEngine.SceneManagement;
 
 public class HackController : MonoBehaviour
 {
@@ -26,10 +27,6 @@ public class HackController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        // Debug.Log(GameObject.Find("Canvas").GetComponent<wordSelection>());
-        // Debug.Log(GameObject.Find("Canvas").GetComponent<wordSelection>().getRandomWords());
-        // Debug.Log(wordGenerator.getRandomWords());
-
 
         Debug.Log(wordGenerator.GetType());
         Debug.Log(GameObject.Find("Canvas").GetComponent<wordSelection>().GetType());
@@ -41,21 +38,9 @@ public class HackController : MonoBehaviour
         choice2Txt = words[1].Item1;
         choice3Txt = words[2].Item1;
         
-
-
-
-        // List<Tuple<string, string>> words = new List<Tuple<string, string>>(GameObject.Find("Canvas").GetComponent<wordSelection>().getRandomWords());
-        // Debug.Log(words.GetType());
-
-        // choice1Txt = words[0].Item1;
-        // choice2Txt = words[1].Item1;
-        // choice3Txt = words[2].Item1;
-        
-
         choice1.onClick.AddListener(ChangeBlankTo1);
         choice2.onClick.AddListener(ChangeBlankTo2);
         choice3.onClick.AddListener(ChangeBlankTo3);
-        // Debug.Log(GameObject.Find("HackButton").GetComponent<Button>());
         GameObject.Find("HackButton").GetComponent<Button>().onClick.AddListener(Hack);
     }
 
@@ -90,6 +75,7 @@ public class HackController : MonoBehaviour
 
     public void Hack()
     {
+        SceneManager.LoadScene("sabotageeScene");
         word = blankSpot.text;
         Debug.Log(word);
     }
