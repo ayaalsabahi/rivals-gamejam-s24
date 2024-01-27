3ad4823cd29ce53a105fe1f016e2f495eb7da08a
Dimitrios <dimitriosbroumas@gmai.com>
2024-01-27 11:49:20 -0500

    Revert "merging meow"
    This reverts commit d7b729abc945ce5137c1a17fb4acfc12103888d3, reversing
    changes made to a82463900d7816b9430ca1b0d9ddf454e255d875.

--- Assets/HackController.cs
+++ Assets/HackController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using UnityEngine.EventSystems;
 
 public class HackController : MonoBehaviour
 {
@@ -15,18 +14,13 @@ public class HackController : MonoBehaviour
     public Button choice3;
     public string choice3Txt;
 
-    public TMP_Text blankSpot;
-
     private wordSelection[] words;
-    
 
 
     // Start is called before the first frame update
     void Start()
     {
-        choice1.onClick.AddListener(ChangeBlankTo1);
-        choice2.onClick.AddListener(ChangeBlankTo2);
-        choice3.onClick.AddListener(ChangeBlankTo3);
+        choice1 = GameObject.Find("Choice1").GetComponent<Button>();
     }
 
     // Update is called once per frame
@@ -37,31 +31,8 @@ public class HackController : MonoBehaviour
 
     void SetButtonText()
     {
-        choice1.GetComponentInChildren<TMP_Text>().text = choice1Txt;
-        choice2.GetComponentInChildren<TMP_Text>().text = choice2Txt;
-        choice3.GetComponentInChildren<TMP_Text>().text = choice3Txt;
+        choice1.GetComponentInChildren<Text>().text = choice1Txt;
+        choice2.GetComponentInChildren<Text>().text = choice2Txt;
+        choice3.GetComponentInChildren<Text>().text = choice3Txt;
     }
-
-    void ChangeBlankTo1()
-    {
-        blankSpot.text = choice1.GetComponentInChildren<TMP_Text>().text;
-    }
-
-    void ChangeBlankTo2()
-    {
-        blankSpot.text = choice2.GetComponentInChildren<TMP_Text>().text;
-    }
-
-    void ChangeBlankTo3()
-    {
-        blankSpot.text = choice3.GetComponentInChildren<TMP_Text>().text;
-    }
-
-
-    void MouseExit()
-    {
-        blankSpot.text = "";
-        Debug.Log("Mouse left.");
-    }
-
 }
