0298a265ca4298b439d69f4713ee88849dab5291
Aya Al Sabahi <ayaalsabahi735@gmail.com>
2024-01-26 22:35:41 -0500

    added word

---
+++ Assets/wordSelection.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wordSelection : MonoBehaviour
+{
+    public (string, string) currWord = ("Hello", "World");
+
+
+
+    
+}
